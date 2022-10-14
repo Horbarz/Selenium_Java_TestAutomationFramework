f07081453731820685d06fb430379954599fc902
horbarz <horbarz007@gmail.com>
2022-10-14 23:31:21 +0100

    Included Screenshot taking functionality

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,17 +1,19 @@
 package base;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import pages.LoginPage;
 import util.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class BaseTests {
@@ -37,6 +39,20 @@ public class BaseTests {
     public void goHome(){
         driver.get("https://the-internet.herokuapp.com/");
         homePage = new HomePage(driver);
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()){
+            var camera = (TakesScreenshot)driver;
+
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try{
+                Files.move(screenshot, new File("resources/screenshots/"+result.getName()+".png"));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
